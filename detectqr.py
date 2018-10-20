88a3933c56ac622f77aa7448669ca78e34774ed0
VisarutDear <42993061+VisarutDear@users.noreply.github.com>
2018-10-20 22:09:27 +0700

    Add files via upload

--- detectqr.py
+++ detectqr.py
@@ -6,9 +6,10 @@
 
 from pyzbar.pyzbar import decode
 from PIL import Image
-res = decode(Image.open('DEAR.JPG'))
-if res:
-    for i in res:
-        print(i.data)
-else:
-    print("no no")
+res = decode(Image.open('outA.png'))
+def abc(res): 
+    if res:
+        for i in res:
+            print(i.data)
+    else:
+        print("no no")
