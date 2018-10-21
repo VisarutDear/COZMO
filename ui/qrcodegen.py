5bd4e21443b1aecc5dd51371408ff45926777fc7
khuna1m <44013169+khuna1m@users.noreply.github.com>
2018-10-21 19:25:59 +0700

    Update qrcodegen.py

--- ui/qrcodegen.py
+++ ui/qrcodegen.py
@@ -1,15 +1,9 @@
 import pyqrcode
 import png
 import json
-from pprint import pprint
 
 with open('data.json') as f:
     data = json.load(f)
 
 qr=pyqrcode.create(str(data[-1]))
 qr.png('qrcode.png',scale=7)
-try :
-    import png
-except:
-    print("")
-    sys.exit('0')
