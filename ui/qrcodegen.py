b941ec2eb83aa93507c1fb9b7d80878e3c91c18f
khuna1m <44013169+khuna1m@users.noreply.github.com>
2018-11-01 14:28:07 +0700

    Update qrcodegen.py

--- ui/qrcodegen.py
+++ ui/qrcodegen.py
@@ -10,5 +10,5 @@ qr = pyqrcode.create(str(data[-1]))
 qr.png('qrcode.png',scale=7)
 
 qr = Image.open('C:/wamp64/www/cozmo_ui/qrcode.png')
-qr.save('C:/wamp64/www/cozmo_ui/templates/static/qrcode.png')
-#there is two qrcode(s) in the same folder as this python file and in static folder
+qr.save('C:/wamp64/www/cozmo_ui/static/qrcode.png')
+#line 13: save QRCODE to static folder - after run this code there will be TWO qrcode.png (1. at the same directory as this .py file 2. in static folder)
