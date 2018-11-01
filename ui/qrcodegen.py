176dd27ab4492954165b33d686e1e8662f9f3c80
khuna1m <44013169+khuna1m@users.noreply.github.com>
2018-11-01 14:32:52 +0700

    Update qrcodegen.py

--- ui/qrcodegen.py
+++ ui/qrcodegen.py
@@ -3,13 +3,14 @@ import png
 import json
 from PIL import Image
 
-with open('data.json') as f:
-    data = json.load(f)
+def generate():
+    with open('data.json') as f:
+        data = json.load(f)
 
-qr = pyqrcode.create(str(data[-1]))
-qr.png('qrcode.png',scale=7)
+    qr = pyqrcode.create(str(data[-1]))
+    qr.png('qrcode.png',scale=7)
 
-qr = Image.open('C:/wamp64/www/cozmo_ui/qrcode.png')
-qr.save('C:/wamp64/www/cozmo_ui/static/qrcode.png')
-# line 13: save QRCODE to static folder
+    qr = Image.open('C:/wamp64/www/cozmo_ui/qrcode.png')
+    qr.save('C:/wamp64/www/cozmo_ui/static/qrcode.png')
+# line 14: save QRCODE to static folder
 # After run this code there will be TWO qrcode.png (1. at the same directory as this .py file 2. in static folder)
