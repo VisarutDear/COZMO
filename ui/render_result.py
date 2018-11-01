2868ee51fbd9e77568d9a553fcf96deec69596fb
khuna1m <44013169+khuna1m@users.noreply.github.com>
2018-11-01 14:34:25 +0700

    Update render_result.py

--- ui/render_result.py
+++ ui/render_result.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template
+import os
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+import qrcodegen
 
 @app.route('/')
 def result():
+    qrcodegen.generate()
     return render_template('result.html')
+
+@app.after_request
+def add_header(response):
+    response.cache_control.max_age = 1
+    response.cache_control.public = True
+    return response
 
 if __name__ == '__main__':
    app.run(debug = True)
